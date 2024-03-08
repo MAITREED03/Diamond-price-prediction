79d2b4ddf8e88549488d2470b27f7d5e42ed657f
MAITREED03 <moitridey8856@gmail.com>
2024-03-08 19:19:36 +0530

    Project

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -10,6 +10,8 @@ from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
 
+
+
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
@@ -18,7 +20,12 @@ if __name__=='__main__':
     data_transformation=DataTransformation()
 
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-
+    
     model_trainer=ModelTrainer()
     model_trainer.initate_model_training(train_arr,test_arr)
 
+    
+
+
+
+    
